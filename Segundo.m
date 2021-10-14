c928eacef42efe7d1caaff9fe7cae37538c09fc8
BradlyAlex <90217754+BradlyAlex@users.noreply.github.com>
2021-10-14 16:49:15 -0500

    Actualización de cosas
    Demostración

--- Segundo.m
+++ Segundo.m
@@ -1,6 +1,6 @@
 % Octave Two Script
 % Title			      : Paginas de un libro
-% Description		  :Script para graficar una funcion 
+% Description		      :Script para graficar una funcion 
 % Authors		      :Diego Romero Antonio, Magali Valencia Clemente, Alexander Bradley Landa Laureano
 % Date			      :20211013
 % Version		      :1
@@ -11,7 +11,7 @@
 % Limpiar variables 
 clear
 
-% Cada una de las páginas de un libro debe tener 600 centímetros cuadrados de área, con márgenes  de  dos centímetros a los  lados y tres centímetros  arriba y  abajo, encuentralas dimensiones de la página para que permita la mayor área impresa posible.
+% Cada una de las páginas de un libro debe tener 600 cm² de área, con márgenes  de  2cm a los  lados y 3cm  arriba y  abajo, encuentre las dimensiones de la página para que permita la mayor área impresa posible.
 
 % Valor de rango de 0..1 en i = 0.1
 X='29.9: 0.001: 29.9';
@@ -21,7 +21,7 @@ f(X,Y);
 f(X-21,9).(Y-27,38);
 
 % Funcion a plotear
-=((X-4).(600/Y - 5));
+f(x)=((X-4).(600/Y - 5));
 
 % Funcion para determinar el valor maximo
 
