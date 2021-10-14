5c1b932118c2af9e5ad1c21c4449cbb6e45e604f
magalivalencia <91564173+magalivalencia@users.noreply.github.com>
2021-10-13 20:20:43 -0500

    modificación 2

--- Segundo.m
+++ Segundo.m
@@ -1,16 +1,16 @@
 % Octave Two Script
-% Title			:Funcion minimo material
-% Description		:Script para graficar una funcion 
-% Authors		:Diego Romero Antonio, Magali Valencia Clemente, Alexander Bradley Landa Laureano
-% Date			:20211013
-% Version		:1
-% Usage			:C:/Users/SERVODOR-RNCANT/Documents
-% Notes			:Requiere aplicacion octave usar en consola preferentemente
+% Title			      : Paginas de un libro
+% Description		  :Script para graficar una funcion 
+% Authors		      :Diego Romero Antonio, Magali Valencia Clemente, Alexander Bradley Landa Laureano
+% Date			      :20211013
+% Version		      :1
+% Usage			      :C:/Users/SERVODOR-RNCANT/Documents
+% Notes			      :Requiere aplicacion octave usar en consola preferentemente
 
-%Limpiar variables 
+% Limpiar variables 
 clear
 
-%Cada una de las páginas de un libro debe tener 600 centímetros cuadrados de área, con márgenes  de  dos centímetros a los  lados y tres centímetros  arriba y  abajo, encuentralas dimensiones de la página para que permita la mayor área impresa posible.
+% Cada una de las páginas de un libro debe tener 600 centímetros cuadrados de área, con márgenes  de  dos centímetros a los  lados y tres centímetros  arriba y  abajo, encuentralas dimensiones de la página para que permita la mayor área impresa posible.
 
 %Valor de rango de (-∞, ∞)
 x =' ∞ : ∞';
